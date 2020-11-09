8f959ddc397d172959a84390ea9d48be882303b9
Satoshi <sato.takara93@gmail.com>
2020-11-09 02:04:22 -0300

    remaking nav-menu

--- cadastro/src/app/app-routing.module.ts
+++ cadastro/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BabysitterFormComponent } from './forms/babysitter-form/babysitter-form.component';
+import { SignUpFormComponent } from './forms/sign-up-form/sign-up-form.component';
 import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'babysitter', component: BabysitterFormComponent }
+  { path: 'babysitter', component: BabysitterFormComponent },
+  {path:'signup',component:SignUpFormComponent}
 ];
 
 @NgModule({
